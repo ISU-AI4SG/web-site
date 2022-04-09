26e3a9bf9d77aa6ebad5ad0b3299ee91b21a060c
Mehmet Uysal <mehmetuysal2000@yandex.com>
2022-04-10 00:00:43 +0300

    0.0.0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   components: {},
+  data() {
+    return {};
+  },
 };
 </script>
